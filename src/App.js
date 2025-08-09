bfd3ef9b4d3ce562151400559f5c19f9d54bd08e
myhome-immobiliare <agenziaimmobiliare.myhome@gmail.com>
2025-08-09 16:48:17 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Benvenuti su My Home Immobiliare</h1>
+      <p>Il tuo sito di annunci immobiliari</p>
+    </div>
+  );
+}
+
+export default App;
